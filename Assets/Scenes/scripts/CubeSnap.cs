a45084a1f107110a867803880b7ff35c67ee77ab
docmcstuffins <66454758+docmcstuffins@users.noreply.github.com>
2022-03-17 16:57:11 -0700

    Cube snap and teleport added

---
+++ Assets/Scenes/scripts/CubeSnap.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeSnap : MonoBehaviour
+{
+    void Start()
+    {
+        Snap();
+    }
+
+    private void Update()
+    {
+    }
+
+    public void Snap()
+    {
+        Debug.Log((Mathf.Round(transform.position.x * 10) / 10, Mathf.Round(transform.position.y * 10) / 10, Mathf.Round(transform.position.z * 10) / 10));
+        transform.position = new Vector3(Mathf.Round(transform.position.x * 10) / 10, Mathf.Round(transform.position.y * 10) / 10, Mathf.Round(transform.position.z * 10) / 10);
+        transform.rotation = new Quaternion(0,0,0,0);
+    }
+}
